74804c3fd9c9e3bc57d4dd32d69504c0178beac6
Davis Vaughan <davis@posit.co>
2026-03-12 12:27:13 -0400

    Use `stdin` from a file approach to be able to use `processx::run()`

--- R/run.R
+++ R/run.R
@@ -1,42 +1,34 @@
 air_run <- function(command, args, ..., stdin = NULL) {
   check_dots_empty0(...)
 
-  has_stdin <- !is.null(stdin)
-
+  # Never with color!
   args <- c(args, "--no-color")
 
-  p <- processx::process$new(
-    command = command,
-    args = args,
-    stdin = if (has_stdin) "|" else NULL,
-    stdout = "|",
-    stderr = "|",
-    encoding = "UTF-8"
-  )
-
-  defer(p$kill())
+  has_stdin <- !is.null(stdin)
 
   if (has_stdin) {
+    # `processx::run()` only supports stdin from a file, but we REALLY want to
+    # use `run()` anyways over managing a `process$new()` manually. It is way
+    # more complicated and we don't get `run()`'s managed event loop that polls
+    # both stdout and stderr simultaneously, which is useful for us because we
+    # want to keep them separated. Ideally one day `processx::run()` will just
+    # let us provide a string for stdin directly, Gabor said he'd like to, but
+    # right now has no way to poll for stdin, or something like that.
+    stdin_file <- tempfile()
     stdin <- enc2utf8(stdin)
-
-    # callr:::rs__write_for_sure
-    while (TRUE) {
-      stdin <- p$write_input(stdin)
-      if (length(stdin) == 0L) {
-        break
-      }
-      Sys.sleep(.1)
-    }
-
-    # Close connection, allowing Air to advance and begin formatting
-    close(p$get_input_connection())
+    writeLines(stdin, stdin_file)
+    defer(unlink(stdin_file))
+  } else {
+    stdin_file <- NULL
   }
 
-  p$wait()
-
-  list(
-    status = p$get_exit_status(),
-    stdout = p$read_all_output(),
-    stderr = p$read_all_error()
+  processx::run(
+    command = command,
+    args = args,
+    stdin = stdin_file,
+    stdout = "|",
+    stderr = "|",
+    encoding = "UTF-8",
+    error_on_status = FALSE
   )
 }
